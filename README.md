8b90bba0b79467ead76a1f40a2e9b9a58371473d
newmin2 <tkdalstm4723@naver.com>
2022-04-16 15:10:45 +0900

    model만~

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # W7_MiniProject_BackEnd
+##
---
+++ src/main/java/com/example/w7_miniproject_backend/domain/Like.java
@@ -0,0 +1,29 @@
+package com.example.w7_miniproject_backend.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name= "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name= "user_id")
+    private User user;
+
+    //repostitory countbypostid
+}
